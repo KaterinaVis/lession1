082e42888f902913bc698cd2b958ead89ef190da
Katerina <katerina4484@mail.ru>
2022-08-07 20:14:00 +0500

    example003_random

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 10;
+﻿int numberA = new Random ().Next(1, 10); // конструкция для случайного чсисла от 1 до 9
+Console.WriteLine(numberA); // чтоб увидеть выбранное случайное число
+int numberB = new Random().Next(1, 10) ;
+Console.WriteLine(numberB); // чтоб увидеть второе выбранное случайное число
 int result = numberA + numberB;
 Console.WriteLine(result);
