c162f87d4230e524170c901b4c84eebefbfc052a
Katerina <katerina4484@mail.ru>
2022-08-07 20:41:50 +0500

    example005_if

---
+++ example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //оператор для если/то, задается условие, тулоуер для перевода регистрова в нижний
+{
+Console.WriteLine("Ура, это же Маша ");
+}
+else //иначе
+{
+    Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
