b318a71ac1526f331d17c58ba624151e8d6a26fe
Katerina <katerina4484@mail.ru>
2022-08-07 22:59:34 +0500

    example008_friends

---
+++ example008_friends/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 1;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 1;
+int dogSpeed = 5;
+int friend = 2;
+int Time = 0;
+
+while(count>10)
+{
+    if(friend == 1)
+    {
+        Time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        Time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    
+    distance = distance-(firstFriendSpeed + secondFriendSpeed)*Time;
+    count = count + 1;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
